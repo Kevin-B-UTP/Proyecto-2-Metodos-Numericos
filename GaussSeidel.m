297d6d89b088a494780f0dde618c9eae6b1af191
Kevin Brennan <82340612+Kevin-B-UTP@users.noreply.github.com>
2021-09-25 01:37:16 -0500

    Add files via upload

---
+++ GaussSeidel.m
@@ -0,0 +1,32 @@
+## Metodo Gauss Seidel
+
+function matriz = gseidel(matriz)
+  clc;
+  #Variables
+  
+  
+  
+  #Encabezado
+   fprintf('\n__________________________________');
+   fprintf('\n     MÉTODO GAUSS SEIDEL');
+   fprintf('\n__________________________________\n');
+  # 
+  
+  
+  
+  
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  endfunction
