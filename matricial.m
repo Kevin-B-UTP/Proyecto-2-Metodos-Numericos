bc2893d902c33e6a3320ae2897ebb97489d56206
EDWAR G <42678417+BATTOZAY@users.noreply.github.com>
2021-09-29 10:52:16 -0500

    Cálculo de la matriz B y Cálculo de x,y,z
    Se agrega un for para separar y calcular matriz B y luego utilizando funciones del octave se calcula x, y, z

--- matricial.m
+++ matricial.m
@@ -6,10 +6,30 @@ function matriz = Mmatricial(matriz)
   fprintf('\nMATRIZ INGRESADA\n\n');
   disp(matriz)
   
-  ## PASO: SEPARACI�N DE PARTES
+  ## PASO: SEPARACIÓN DE PARTES
         for fila = 1 : 3
           for columna = 1 : 3
                 MA(fila,columna) = matriz(fila,columna);
           endfor
         endfor
-endfunction
+        
+      ## SALIDA MATRIZ A
+        fprintf('\nMATRIZ A\n');
+        disp(MA)
+        
+        columna = 4;
+        for fila = 1 : 3
+            MB(fila,1)= matriz(fila,columna);
+        endfor
+        
+      ## SALIDA MATRIZ B
+        fprintf('\nMATRIZ B\n');
+        disp(MB)
+      
+      fprintf('\nRESPUESTAS, x, y, z\n');
+      
+  ## CÁLCULO DE LAS INCÓGNITAS
+  inv(MA)* MB
+  fprintf('\n\n\n\n');
+
+ endfunction
