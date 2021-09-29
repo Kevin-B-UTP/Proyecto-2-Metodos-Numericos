5adbed6ac564d25a256ad6a2f30c315412c099e0
Kevin Brennan <82340612+Kevin-B-UTP@users.noreply.github.com>
2021-09-29 13:21:20 -0500

    Add files via upload

--- GaussSeidel.m
+++ GaussSeidel.m
@@ -1,32 +1,6 @@
 ## Metodo Gauss Seidel
 ###REVISAR
 function matriz = gseidel(matriz, tolerancia)
-  clc;
-  #Variables
-  signo= -1;
-  iteracion=0;
-  x=0;
-  y=0;
-  z=0;
-  Erx=999;
-  Ery=999;
-  Erz=999;
-  #Despeje 
-  while (Erx > tolerancia || Ery > tolerancia || Ery > tolerancia)
-  xold=x;
-  yold=y;
-  zold=z;
-  x = matriz(1,3)+(signo * matriz(1,2)*y)+(signo*matriz(1,3)*x)/(matriz(1,1))
-  y = ( (signo* matriz(2,1)*x) + (signo * matriz(2,3)*z) + matriz (2,4) ) / matriz (2,2)
-  z = ( (signo* matriz(3,1)*x) + (signo * matriz(3,2)*y) + matriz (3,4) ) / matriz (3,3)
-    if iteracion > 1
-    Erx= abs((x-xold)) 
-    Ery= abs((y-yold)) 
-    Erz= abs((z-zold)) 
-    endif
-  iteracion = iteracion + 1;
-  endwhile
-  clc;
   #Encabezado
   clc;
   fprintf('\n__________________________________');
@@ -34,20 +8,49 @@ function matriz = gseidel(matriz, tolerancia)
   fprintf('\n__________________________________\n');
   #Matriz 
   fprintf('\nMatriz Ingresada\n');
+  fprintf('\n__________________________________\n');
   disp(matriz);
-  fprintf('\n__________________________________\n');
-  fprintf('\n');
   fprintf('\n__________________________________');
-  fprintf('\nMÉTODO GAUSS SEIDEL - RESULTADOS');
+  #Variables
+  signo= -1;
+  iteracion=1;
+  x=0;
+  y=0;
+  z=0;
+  Erx=1000;
+  Ery=1000;
+  Erz=1000;
+  #Despeje 
+  while (Erx > tolerancia && Ery > tolerancia && Ery > tolerancia)
+  x = (matriz(1,4)+((signo)*(matriz(1,2))*y)+ ((signo)*(matriz(1,3))*z))/(matriz(1,1));
+  y = (matriz(2,4)+((signo)*(matriz(2,1))*x)+ ((signo)*(matriz(2,3))*z))/(matriz(2,2));
+  z = (matriz(3,4)+((signo)*(matriz(3,1))*x)+ ((signo)*(matriz(3,2))*y))/(matriz(3,3));
+  if (iteracion == 1)
   fprintf('\n__________________________________\n');
-  fprintf('\nNUMERO DE ITERACIONES %i', iteracion);
+  fprintf('\nITERACION No. %2i', iteracion);
   fprintf('\nX= %f', x);
   fprintf('\nY= %f', y);
   fprintf('\nZ= %f', z);
   fprintf('\n__________________________________');
-  fprintf('\n     PORCENTAJE DE ERROR');
-  fprintf('\nEr_x= %f', Erx);
-  fprintf('\nEr_y= %f', Ery);
-  fprintf('\nEr_z= %f', Erz);
-  fprintf('\n__________________________________\n');
+  else 
+    Erx= abs(x-xold); 
+    Ery= abs(y-yold); 
+    Erz= abs(z-zold);
+    fprintf('\n__________________________________\n');
+    fprintf('\nITERACION No. %2i', iteracion);
+    fprintf('\nX= %f', x);
+    fprintf('\nY= %f', y);
+    fprintf('\nZ= %f', z);
+    fprintf('\n__________________________________');
+    fprintf('\nPORCENTAJE DE ERROR');
+    fprintf('\nEr_x= %f', Erx);
+    fprintf('\nEr_y= %f', Ery);
+    fprintf('\nEr_z= %f', Erz);
+    fprintf('\n__________________________________\n');
+  endif
+  iteracion = iteracion + 1;
+  xold=x;
+  yold=y;
+  zold=z;
+  endwhile
 endfunction
